dc6ce91ff826bf78a96264f520f99cca8a16c0c0
David Smith <getdavemail@gmail.com>
2011-06-21 09:19:41 +0100

    Core timing, delegation and transition functionality implemented

--- js/site.js
+++ js/site.js
@@ -2,8 +2,13 @@
 	
 	
 	show = new protoShow('myshow1',{
-		interval	:	2000,
-		captions	: 	true
+		interval		:	2000,
+		captions		: 	true,
+		/*transitionType	:   function() {
+					console.log("My custom trans");	
+				},*/
+		transitionType	: "fade", 
+		cc				: 	false
 	});
 
 	/*$('myshow1') && $('myshow1').protoShow({
--- protoshow.js
+++ protoshow.js
@@ -1,5 +1,5 @@
 /*  ProtoShow JavaScript slide show, 
- *	v 0.6 (alpha) - 12/06/11
+ *	v 0.7 (beta) - 12/06/11
  *  Copyright(c) 2011 David Smith (web: http://www.aheadcreative.com; twitter: @get_dave)
  *
  *  This work is licenced under the Creative Commons Attribution-No Derivative Works 3.0 Unported License. 
@@ -11,6 +11,15 @@
  *
  *--------------------------------------------------------------------------*/
 
+/*
+
+Show "swap" process
+
+1) this.play() initialises a timer to run every "x" seconds
+
+
+
+*/
 
 
 
@@ -27,9 +36,10 @@ var protoShow = Class.create({
 			selector			: ".slide",					
 			interval			: 3000,
 			initialSlide		: 1,
-			mode				: 'forward',
-			autoplay			: true,
+			mode				: 'backward',
+			autoPlay			: true,
 			autoRestart			: true,
+			transitionType		: "slide",
 			transitionTime		: 1.5,
 			manTransitionTime	: 0.5,
 			stopText			: "Pause",
@@ -49,17 +59,33 @@ var protoShow = Class.create({
 		
 
 
-		// get/set various variables
+		// get/set various options
 		this.element 			= 	$(element);											// DOM element that contains the slideshow
 		this.slides 			= 	this.element.select(this.options.selector);			// Elements that are to be the "Slides"		
 		this.slidesLength		=	this.slides.size();		// Total number of Slides
 		this.interval 			= 	this.options.interval;								// Interval delay between swapping the Slides
 		this.currentSlideID 	= 	this.options.initialSlide - 1;		
-		this.nextSlideID		=	this.currentSlideID++;
+		this.nextSlideID		=	this.currentSlideID + 1;
 		this.mode				= 	this[this.options.mode];							// Get play "mode" (forward, backward, random...etc)
-		this.masterTimer		=	false;
+		this.autoPlay			=	this.options.autoPlay;
 
-		// let's get things going!		
+		if (typeof(this.options.transitionType) == "function") {
+			// If function then user has passed in custom transition function to be used
+			this.transitionType		=	this.options.transitionType;
+		} else {
+			this.transitionType		=	this[this.options.transitionType];
+		}
+
+
+		
+
+		// define variables before use
+		this.masterTimer		=	false;
+		this.animating			=	false;	// boolean for "animating" status
+		this.loopCount			=	0;
+
+		// let's get things going!
+		this.slides[this.currentSlideID].show().addClassName('active-slide');		
 		this.play();
 		
 	},
@@ -69,40 +95,92 @@ var protoShow = Class.create({
 	------------------------------------------------*/
 
 	play: function() {
+		console.info("Loop count: " + this.loopCount)
+		console.info("Master timer is: " + this.masterTimer);
+		// Role: Starts the show and initialises master timer
 		console.log("Playing");
-		this.reportSlides();
+		
 		var _this = this;		
 		this.masterTimer	=	new PeriodicalExecuter(function(pe) {
 		  	_this.mode();		    
 		}, this.interval/1000);
+		this.loopCount++;
 	},
 
 	stop: function() {
-		// Entirely stops the show and clears the timer
+		// Completely stops the show and clears the master timer
 		var _this = this;
 		console.log("Stopping");
 		_this.masterTimer.stop();
 		_this.masterTimer = null;
+
 	},
 
 	forward: function() {
-		// Runs slideshow "forwards"
+		// Role: Runs slideshow "forwards"
 		console.log("Forward");
 		this.goMaster( this.currentSlideID + 1 );
 	},
 
 	backward: function() {
-		// Runs slideshow "backwards"
+		// Role: Runs slideshow "backwards"
 		console.log("Backward");		
 		this.goMaster( this.currentSlideID - 1 );	
 	},
 
 	goMaster: function(next) {
-		// Master function - controls delegation of slide swapping	
-		console.log("Swapping slides: C: " + this.currentSlideID + " for N: " + next);
-		this.setNextIndex(next);
+		// Role: Master function - controls delegation of slide swapping	
+		var _this = this;
+		this.stop();
+		this.toggleAnimating(true);
+		
+		this.setNextIndex(next);  // set this.nextSlideID correctly		
+		this.reportSlides();
 
+
+		this.transitionType(this.slides[this.currentSlideID],this.slides[this.nextSlideID], {
+			transitionFinish	:	function() {	// pass a callback to ensure play can't resume until transition has completed
+				_this.toggleAnimating(false);
+				_this.slides[_this.currentSlideID].removeClassName('active-slide');
+				_this.slides[_this.nextSlideID].addClassName('active-slide');
+				
+				_this.currentSlideID = _this.nextSlideID;	// update current slide to be the slide we're just moved to
+				if (_this.autoPlay) {
+					_this.play();
+				}				
+			}
+		});
+		
+		
+
+		
 	},
+
+
+	/* TRANSITION FUNCTIONS
+	------------------------------------------------*/
+
+	fade: function(current,next,opts) {
+		// Role: Transition function
+		// Type: Fade - fades slides in and out
+		next.show();
+		current.fade({
+			afterFinish: function() {
+				return opts.transitionFinish();
+			}
+		});
+
+		
+			
+	},
+
+	slide: function(current,next,opts) {
+		// Role: Transition function
+		// Type: Slider - slides slides across the screen
+		console.log("Sliding");
+	},
+
+	
 
 
 
@@ -113,22 +191,49 @@ var protoShow = Class.create({
 		return this.masterTimer != null;
 	},
 
+	isAnimating: function() {
+		return this.animating;
+	},
+
+	toggleAnimating: function(boolean) {
+		// Role: toggles var to say whether animation is in progress and manipulates DOM
+		this.animating = boolean;
+		if (boolean) {
+			this.element.addClassName("animating");	
+		} else {
+			this.element.removeClassName("animating");
+		}
+	},
+
 	setNextIndex: function(next) {
+		// Role: Decides on direction and ensures within bounds
+		
+		if(next === undefined) { // Ensure "next" has a value
+			next = this.currentSlideID+1;
+		}
+
+		// Ensure we're within bounds
+		if (next >= this.slidesLength) {
+			next = 0;
+		} else if (next < 0 ){
+			next = this.slidesLength-1;
+		}
+
 		this.nextSlideID = next;	
 	},
 
 
 	
-	
-
-
 	/* LOGGING FUNCTIONS
 	------------------------------------------------*/
 
 	reportSlides: function() {
-		console.log(this.currentSlideID);
-		console.log(this.nextSlideID);	
+		console.log("Current slide: " + this.currentSlideID);
+		console.log("Next slide: " + this.nextSlideID);	
 	},
+
+
+
 
 	cc: function() {
 		// catches the comma
